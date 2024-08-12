051f5c07058d2eb38f350a4a7dbcf08144ccdfb8
c-tollison <tollisoncarson@gmail.com>
2024-08-12 11:21:07 -0400

    fix mobile styling

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,7 +4,12 @@ export default function Footer() {
     return (
         <footer className="footer">
             <a href="https://github.com/c-tollison" target="_blank">
-                <svg width="98" height="96" xmlns="http://www.w3.org/2000/svg">
+                <svg
+                    width="98"
+                    height="96"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon"
+                >
                     <path
                         fillRule="evenodd"
                         clipRule="evenodd"
@@ -13,6 +18,31 @@ export default function Footer() {
                     />
                 </svg>
             </a>
+
+            <a
+                href="https://github.com/c-tollison/carson-dev"
+                target="_blank"
+                className="center"
+            >
+                About this site
+                <svg
+                    width="26px"
+                    height="26px"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        id="Vector"
+                        d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11"
+                        stroke="#f0f0f0"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    />
+                </svg>
+            </a>
+
             <a
                 href="https://www.linkedin.com/in/carson-tollison/"
                 target="_blank"
@@ -22,6 +52,7 @@ export default function Footer() {
                     width="98"
                     height="96"
                     viewBox="0 0 24 24"
+                    className="icon"
                 >
                     <path
                         fillRule="evenodd"
