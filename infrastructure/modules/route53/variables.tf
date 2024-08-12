2c88d94e9e11852ba3fea4b90fc156a15998126f
c-tollison <tollisoncarson@gmail.com>
2024-08-11 22:18:00 -0400

    gitignore terraform, fix vars

--- infrastructure/modules/route53/variables.tf
+++ infrastructure/modules/route53/variables.tf
@@ -1,6 +1,5 @@
 variable "route53-zone-id" {}
 variable "domain-validation-options" {}
-variable "route53-zone-id" {}
 variable "domain-name" {}
 variable "cloudfront-subdomain-domain-name" {}
 variable "cloudfront-root-domain-name" {}
