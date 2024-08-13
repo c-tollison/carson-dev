d5f3978feac532d50a55468758516f24316f2e64
c-tollison <tollisoncarson@gmail.com>
2024-08-13 00:06:05 -0400

    experience revisions

--- src/routes/experience/experience.tsx
+++ src/routes/experience/experience.tsx
@@ -9,19 +9,20 @@ export default function Experience() {
             companyOrProjectName: "ChiroHD",
             jobTitle: "Software Engineer",
             location: "Remote",
-            date: "Nov '21 - May '22, Aug '22 - May '23, Aug '23 - Present",
+            date: "Nov '21 - Present",
             details: [
-                "Enhanced EOB processing by redesigning the ERA parser to leverage S3 triggers for multi-EOB handling and auto-populating dashboards, reducing manual entry by 80% and expediting payment processing.",
-                "Led the integration of end-to-end testing with Cypress into CI/CD pipelines, cutting manual testing by 50% and accelerating release cycles by 30%.",
+                "Enhanced EOB processing using SFTP file servers and S3 triggers, reducing manual entry by 80% and expediting payment processing. Bringing insurance processing to over 700 offices.",
+                "Led integration of end-to-end testing, cutting manual testing by 50% and accelerating release cycles by 30% using CircleCi for CI/CD and Cypress for testing.",
                 "Integrated Twilio opt-in messaging and developed a robust texting environment, doubling testing efficiency, ensuring government compliance, and streamlining communication.",
-                "Contributed to key projects across multiple work periods, balancing academic development with professional experience.",
+                "Developed healthcare management software using TypeScript, Node.js, Angular, and PostgreSQL.",
+                "Implemented and maintained AWS infrastructure, leveraging services like Lambda, S3, and RDS.",
             ],
         },
         {
             companyOrProjectName: "Amazon",
             jobTitle: "Software Development Engineer Intern",
             location: "Seattle, WA",
-            date: "May - Aug '22, May - Aug '23",
+            date: "May - Aug '22 & '23",
             details: [
                 "Engineered scalable automation solutions using Python and Java, enhancing workflow efficiency by 40% and improving system reusability across multiple teams.",
                 "Utilized AWS services (Lambda, SQS, DynamoDB) to build and optimize a real-time data processing project, ensuring consistent deployments with AWS CDK.",
@@ -40,7 +41,7 @@ export default function Experience() {
             details: [
                 "Led front-end development and Scrum processes for a 5-person team, designing the UI with Figma.",
                 "Developed a web app for creating contracting bid templates using React and Python/Flask, streamlining bid creation by 40%.",
-                "Deployed front-end on AWS Elastic Beanstalk and back-end serverless via Lambda and Zappa, with secure login through AWS Cognito",
+                "Deployed front-end on AWS Elastic Beanstalk and back-end serverless via Lambda and Zappa, with secure login through AWS Cognito.",
             ],
         },
     ];
