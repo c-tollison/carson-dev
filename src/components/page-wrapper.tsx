0bc604b7e40055a97551759587f23ce761089528
c-tollison <tollisoncarson@gmail.com>
2025-04-09 09:03:29 -0400

    scroll issues

--- src/components/page-wrapper.tsx
+++ src/components/page-wrapper.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import ScrollToTop from './scroll-to-top';
 
 export default function PageWrapper({ children }: { children: React.ReactNode }) {
     const [isVisible, setIsVisible] = useState(false);
@@ -9,6 +10,7 @@ export default function PageWrapper({ children }: { children: React.ReactNode })
 
     return (
         <div className={`transition-opacity duration-500 ease-in-out ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+            <ScrollToTop />
             {children}
         </div>
     );
