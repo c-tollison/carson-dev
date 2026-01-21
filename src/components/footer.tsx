c1e25c7e70be2ac674c7c08742d9d7fd57665fed
c-tollison <tollisoncarson@gmail.com>
2026-01-21 08:20:07 -0500

    Auto calc copy right

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className='flex justify-center items-center w-full border-t py-4 border-border text-sm'>
-            © 2025 Carson Tollison
+            © {new Date().getFullYear()} Carson Tollison
         </footer>
     );
 }
