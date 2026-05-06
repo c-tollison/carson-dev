4f15f321d43a3edb1c3b5a8fecdaca6c6b3528a7
c-tollison <tollisoncarson@gmail.com>
2026-05-06 10:40:53 -0400

    remove top nav

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom';
-import NavBar from '../components/nav-bar';
 import Footer from '../components/footer';
 
 export default function Root() {
     return (
         <div className='min-h-screen w-screen bg-background px-6 md:px-8 flex justify-center font-body text-foreground transition-colors duration-300 ease-in-out'>
             <div className='max-w-5xl w-full flex flex-col min-h-screen'>
-                <NavBar />
                 <main className='flex-grow py-10'>
                     <Outlet />
                 </main>
